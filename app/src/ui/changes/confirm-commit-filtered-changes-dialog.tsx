13d3bb50885c8ff89cd73faad8232e98186d01f8
f1ow <27006147+f1owkang@users.noreply.github.com>
2026-07-03 16:55:12 +0800

    Localize commit confirmation dialog and filter options

--- app/src/ui/changes/confirm-commit-filtered-changes-dialog.tsx
+++ app/src/ui/changes/confirm-commit-filtered-changes-dialog.tsx
@@ -4,6 +4,7 @@ import { Row } from '../lib/row'
 import { OkCancelButtonGroup } from '../dialog/ok-cancel-button-group'
 import { Checkbox, CheckboxValue } from '../lib/checkbox'
 import { LinkButton } from '../lib/link-button'
+import { t, platformT } from '../../lib/i18n'
 
 interface IConfirmCommitFilteredChangesProps {
   readonly onCommitAnyway: () => void
@@ -33,9 +34,7 @@ export class ConfirmCommitFilteredChanges extends React.Component<
       <Dialog
         id="hidden-changes"
         type="warning"
-        title={
-          __DARWIN__ ? 'Commit Filtered Changes?' : 'Commit filtered changes?'
-        }
+        title={platformT('changes.confirmFiltered.title')}
         onSubmit={this.onSubmit}
         onDismissed={this.props.onDismissed}
         role="alertdialog"
@@ -43,16 +42,15 @@ export class ConfirmCommitFilteredChanges extends React.Component<
       >
         <DialogContent>
           <p id="confirm-commit-filtered-changes-message">
-            You have a filter applied. There are{' '}
+            {t('changes.confirmFiltered.messagePrefix')}{' '}
             <LinkButton onClick={this.showFilesToBeCommitted}>
-              hidden changes
+              {t('changes.confirmFiltered.hiddenChanges')}
             </LinkButton>{' '}
-            that will be committed. Are you sure you want to commit these
-            changes?
+            {t('changes.confirmFiltered.messageSuffix')}
           </p>
           <Row>
             <Checkbox
-              label="Do not show this message again"
+              label={t('changes.confirmFiltered.doNotShow')}
               value={
                 this.state.askForConfirmationOnCommitFilteredChanges
                   ? CheckboxValue.Off
@@ -65,8 +63,8 @@ export class ConfirmCommitFilteredChanges extends React.Component<
         <DialogFooter>
           <OkCancelButtonGroup
             destructive={true}
-            okButtonText={__DARWIN__ ? 'Commit Anyway' : 'Commit anyway'}
-            cancelButtonText={'Cancel'}
+            okButtonText={platformT('changes.confirmFiltered.commitAnyway')}
+            cancelButtonText={t('common.cancel')}
             onCancelButtonClick={this.props.onDismissed}
           />
         </DialogFooter>
